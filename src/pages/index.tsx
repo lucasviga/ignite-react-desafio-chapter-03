581cae898c3a5999f1320da857aa5e2fab250bd4
Lucas Viga <lucasviga12@gmail.com>
2022-02-16 02:38:36 -0400

    add styles to page home, install react-icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetStaticProps } from 'next';
+import { GetStaticProps, NextPage } from 'next';
+import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +25,99 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <main className={styles.container}>
+      <div className={styles.post}>
+        <h1>How to use hooks in React</h1>
+        <p>Thinking about synchronization instead of lifecycles.</p>
+
+        <div>
+          <span>
+            <AiOutlineCalendar />
+            19 April 2021
+          </span>
+          <span>
+            <AiOutlineUser />
+            Lucas Viga
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.post}>
+        <h1>How to use hooks in React</h1>
+        <p>Thinking about synchronization instead of lifecycles.</p>
+
+        <div>
+          <span>
+            <AiOutlineCalendar />
+            19 April 2021
+          </span>
+          <span>
+            <AiOutlineUser />
+            Lucas Viga
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.post}>
+        <h1>How to use hooks in React</h1>
+        <p>Thinking about synchronization instead of lifecycles.</p>
+
+        <div>
+          <span>
+            <AiOutlineCalendar />
+            19 April 2021
+          </span>
+          <span>
+            <AiOutlineUser />
+            Lucas Viga
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.post}>
+        <h1>How to use hooks in React</h1>
+        <p>Thinking about synchronization instead of lifecycles.</p>
+
+        <div>
+          <span>
+            <AiOutlineCalendar />
+            19 April 2021
+          </span>
+          <span>
+            <AiOutlineUser />
+            Lucas Viga
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.post}>
+        <h1>How to use hooks in React</h1>
+        <p>Thinking about synchronization instead of lifecycles.</p>
+
+        <div>
+          <span>
+            <AiOutlineCalendar />
+            19 April 2021
+          </span>
+          <span>
+            <AiOutlineUser />
+            Lucas Viga
+          </span>
+        </div>
+      </div>
+
+      <button
+        type="button"
+        className={styles.loadMoreButton}
+        onClick={() => console.log('pressed')}
+      >
+        Load more posts
+      </button>
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
